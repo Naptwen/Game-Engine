9209532b71c4c05f108f647b8b63dd95230b8517
Naptwen <47798805+Naptwen@users.noreply.github.com>
2021-07-10 23:09:09 -0700

    Update Python_obj.py

--- C/Python_obj.py
+++ C/Python_obj.py
@@ -16,11 +16,11 @@ CHello._fields_ = [
         ("normals_size", POINTER(c_uint)),
         ("text_coor_list", POINTER(POINTER(c_double))),
         ("text_coor_size", POINTER(c_uint)),
-        ("ind_v_list", POINTER(POINTER(c_double))),
+        ("ind_v_list", POINTER(POINTER(c_uint))),
         ("ind_v_size", POINTER(c_uint)),
-        ("ind_n_list", POINTER(POINTER(c_double))),
+        ("ind_n_list", POINTER(POINTER(c_uint))),
         ("ind_n_size", POINTER(c_uint)),
-        ("ind_t_list", POINTER(POINTER(c_double))),
+        ("ind_t_list", POINTER(POINTER(c_uint))),
         ("ind_t_size", POINTER(c_uint)),
         ("mtllib", c_char_p,)
     ]
@@ -36,19 +36,38 @@ mydll.CHello_loading.argtypes = [POINTER(CHello), c_char_p]
 mydll.CHello_loading.restype = c_void_p
 
 
-class cello():
+class Loader:
     obj = POINTER(CHello)
+    sub_count = 0
     vertices = []
+    normals = []
+    ind_v = []
+
+
     def __init__(self):
         self.obj = mydll.CHello_new(1)
+
 
     def loading(self, file_name):
         file_name = create_string_buffer(file_name.encode('utf-8'))
         mydll.CHello_loading(self.obj, file_name)
-        print(self.obj[0].sub_count)
+        self.sub_count = self.obj[0].sub_count
         for index in range(self.obj[0].sub_count):
             vec_size = self.obj[0].vertices_size[index]
-            new_vec = self.obj[index].vertices_list[index][0:vec_size]
+            new_vec = self.obj[0].vertices_list[index][0:vec_size]
             self.vertices.append(new_vec)
-            print(self.vertices)
+
+        for index in range(self.obj[0].sub_count):
+            norm_size = self.obj[0].normals_size[index]
+            new_norm = self.obj[0].normals_list[index][0:norm_size]
+            self.normals.append(new_norm)
+
+        for index in range(self.obj[0].sub_count):
+            ind_v_size = self.obj[0].ind_v_size[index]
+            new_ind_v = self.obj[0].ind_v_list[index][0:ind_v_size]
+            print(self.obj[0].ind_v_list[index][0])
+            print(new_ind_v)
+            print("asdasdasdasdasdasdasdasdasdass")
+            self.ind_v.append(new_ind_v)
+        print('1',self.ind_v)
 
